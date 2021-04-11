369fde667ff223e8b4cf15ee8b897ee41145ea43
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2021-04-11 13:47:47 +0200

    add use_logo cmd

--- data-raw/setup.R
+++ data-raw/setup.R
@@ -1,2 +1,3 @@
 devtools::source_gist("654b9a85e2f005638d639f3e2f72bbb2")
 create_mc_package("mc", "Mickaël Canouil", "Bunch of functions mostly for my personal use.", "Mickaël Canouil")
+usethis::use_logo("https://raw.githubusercontent.com/mcanouil/hex-stickers/main/PNG/mc.png")
