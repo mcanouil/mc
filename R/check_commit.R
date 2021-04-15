cb358c969901909ccb16baa44a1afcb4670a170d
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2021-04-15 16:52:40 +0200

    fix output with files

--- R/check_commit.R
+++ R/check_commit.R
@@ -21,26 +21,23 @@ check_commit <- function(
   dta <- dta[sapply(dta[["path"]], function(x) !inherits(try(gert::git_find(path = x), silent = TRUE), "try-error")), ]
   dta <- dta[!grepl("^SB_", basename(dta[["path"]])), ]
   dta[["commit"]] <- sapply(dta[["path"]], function(x) nrow(gert::git_diff(repo = x)) > 0)
-
   if (commit) {
     sapply(
       X = dta[dta[["commit"]], ][["path"]],
       FUN = function(x) gert::git_commit_all(message = "automatic commit", repo = x)
     )
   }
-
   if (push) {
     sapply(
       X = dta[["path"]],
       FUN = function(x) try(gert::git_push(repo = x))
     )
   }
-
   out <- dta[dta[["commit"]], c("path", "user", "group", "access_time")]
-
-  out[["files"]] <- lapply(out[["path"]], function(x) gert::git_status(repo = x))
-
-  out[, names(out)[c(1, ncol(out), 2:(ncol(out) - 1))]]
+  files_tmp <- lapply(out[["path"]], function(x) gert::git_status(repo = x))
+  out <- cbind(out[rep(seq_along(files_tmp), sapply(files_tmp, nrow)), ], do.call("rbind", files_tmp))
+  class(out) <- c("tbl_df", "tbl", "data.frame")
+  out[c("path", "file", "status", "staged", "user", "group", "access_time")]
 }
 
 #' @rdname check_commit
